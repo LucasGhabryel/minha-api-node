11ba88edfc820d41851e6fc79e2dd569110a9df4
BrunoRafael14 <brunorafaelfilho10@gmail.com>
2026-04-15 21:19:02 -0300

    Adicionando rota get para todos os registros de links Afiliados

--- src/routes/linkRoutes.js
+++ src/routes/linkRoutes.js
@@ -1,9 +1,10 @@
-import express from 'express'
-import { listarLinkAfiliados, criarLinkAfiliados } from '../controllers/linkController.js'
+import express from "express"
+import { listarLinkAfiliadosGeral, listarLinkAfiliadosId, criarLinkAfiliados } from '../controllers/linkController.js'
 import autenticar from '../middlewares/autenticar.js'
 
 const router = express.Router()
 
-router.get('/links/:id', autenticar, listarLinkAfiliados)
+router.get('/links', autenticar, listarLinkAfiliadosGeral)
+router.get('/links/:id', autenticar, listarLinkAfiliadosId)
 router.post('/links', autenticar, criarLinkAfiliados)
 export default router
